fd08a40a822f0d0b2d90ec87fabdb4aa6cc37758
김유나 <20162921@gm.hannam.ac.kr>
2024-02-19 13:13:12 +0900

    Design: Mui 라이브러리 설치 및 기본 적용 - 홈
    김유나(24.02.19)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+import { Button, Typography, Container, Box } from "@mui/material";
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Tic tac toe game</h1>
-      <Link to="/setup">게임 시작</Link>
-      <Link to="/records">기록된 게임 보기</Link>
-    </div>
+    <Container maxWidth="sm">
+      <Box textAlign="center" mt={5}>
+        <Typography variant="h2" gutterBottom>
+          Tic Tac Toe Game
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          component={RouterLink}
+          to="/setup"
+          sx={{ margin: 1 }}>
+          게임 시작
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          component={RouterLink}
+          to="/records"
+          sx={{ margin: 1 }}>
+          기록된 게임 보기
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
